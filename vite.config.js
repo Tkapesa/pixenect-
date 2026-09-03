3bdb1dfcadb669db7f5c3c9795d73535ad99e238
kapes <kapes@example.com>
2026-09-04 00:01:16 +0300

    Fix build paths for GitHub and portable deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pixenect-/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/pixenect-/' : './',
   plugins: [react()],
 })
